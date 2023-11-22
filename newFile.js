296173af51e44d758afa89875b057eb254afce5a
switchesto <albinfedrick@gmail.com>
2023-11-22 08:51:28 +0530

    first commit

---
+++ newFile.js
@@ -0,0 +1,13 @@
+
+    function myFunction() {variable} x, y;
+
+    // Get the value of the input field with id="firstnumber"
+    x = document.getElementById("firstnumber").value;
+
+    // Get the value of the input field with id="secondnumber"
+    x = document.getElementById("secondnumber").value;
+
+    // If x is Not a Number or less than one or greater than 10
+
+    document.getElementById("answer").innerHTML = x + y;
+}
